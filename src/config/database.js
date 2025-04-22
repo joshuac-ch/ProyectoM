42c4ea728186b47e0f13abe8c8c6c294a80fafc9
joshua <condorenajoshua98@gmail.com>
2025-04-21 19:29:31 -0500

    Se hizo el cambio de hora a UTC para el backend

--- src/config/database.js
+++ src/config/database.js
@@ -5,5 +5,5 @@ dotenv.config()
 module.exports = sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: "mysql",
-    logging: false, // Desactiva logs de SQL
+    logging: false, // Desactiva logs de SQL   
 });
